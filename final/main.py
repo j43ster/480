40070341caca69bd2a21cd38512e384bac0af847
Jeffrey Forrester <jeffrey_forrester@yahoo.com>
2014-11-20 03:53:37 -0800

    used nltk to turn an english sentence into a FOL statement!

--- final/main.py
+++ final/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 import lifting_rules
+import parser
 
 #lifting_rules.test()
 
@@ -31,10 +32,36 @@ while(True):
    # split sentences up 
    # for each sentence in response, send it through nltk
    # to try to pull out FOL queries, facts, and rules
-   print kb.query(sentence) 
+   #print kb.query(sentence) 
      
    # send FOL through rules moduqe 
-    
+   if (sentence.startswith("try: ")):
+      print "going to try to understand an english sentence"
+      sentence = sentence.partition("try: ")[2]
+      processed = parser.ie_preprocess(sentence)
+      print processed
+      if (sentence.lower().find("i ") != -1): 
+         nouns = [i for (i, j) in processed[0] if j == "NN"]
+         print str(nouns)
+         for noun in nouns:
+            action = "%s(%s)" % (noun, kb_name)
+            print action
+            kb.tell(action)
+   elif (sentence[-1] == "?"):
+      result = kb.ask(sentence[:-1])
+
+      if (result == []):
+         print "robot: False"
+      elif (len(result) == 1 and result[0] == '[]'):
+         print "robot: True"
+      else:
+         print "robot: %s" % str(result)
+
+   else:
+      if (sentence[-1] == "."):
+         sentence = sentence[:-1]
+
+      kb.tell(sentence)
 
    # any responses to the client       
 
