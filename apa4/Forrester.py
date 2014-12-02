7f755405b89635afcd0ed108d24359ac42e3c865
Jeff Forrester <jeffrey_forrester@yahoo.com>
2014-12-02 11:08:31 -0800

    fixed apa4 errors

--- apa4/Forrester.py
+++ apa4/Forrester.py
@@ -17,12 +17,12 @@ def Forrester4 (hist=[], score=[], query = False):
          # look at last few turns and choose what they chose more of
          c_count = 0
          d_count = 0
-         for i in {0...N}:
-             if hist[-1][-i] == 'C':
+         for i in range(N-1):
+             if hist[-1][len(hist[-1])-i] == 'C':
                 c_count = c_count + 1
              else:
                 d_count = d_count + 1            
          if d_count > c_count:
             return 'D'
-         else
+         else:
             return 'C'
