fcd09faa06168190cb7deacd2ed446bd711f574e
Jeff Forrester <jeffrey_forrester@yahoo.com>
2014-12-02 10:59:43 -0800

    changed apa4 strategy

--- apa4/Forrester.py
+++ apa4/Forrester.py
@@ -3,10 +3,26 @@
 ForresterStore = {}
 
 def Forrester4 (hist=[], score=[], query = False):
+
+   N = 100
+
    if query:
       return ["J4","Almost done"]
    else:
-      if (len(hist) == 0):
+      if (len(hist) < N):
          return 'C'
-      else:
+      elif (len(hist) < 2*N):
          return hist[-1][-1]
+      else:
+         # look at last few turns and choose what they chose more of
+         c_count = 0
+         d_count = 0
+         for i in {0...N}:
+             if hist[-1][-i] == 'C':
+                c_count = c_count + 1
+             else:
+                d_count = d_count + 1            
+         if d_count > c_count:
+            return 'D'
+         else
+            return 'C'
